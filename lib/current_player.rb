1114636d702c4ee275aca172d643b0e7321795e4
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-28 00:30:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,10 @@
-
+#turn count needs to take in a board and count the number of spaces with an x or an o
+def turn_count (board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
